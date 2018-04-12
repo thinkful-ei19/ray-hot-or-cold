35327dbe29d37374711421b592e2e0be35ef8523
Ray Yeung <raymond.zj.yeung@gmail.com>
2018-04-12 09:46:26 -0700

    updated root component with methods

--- src/components/game.js
+++ src/components/game.js
@@ -9,9 +9,31 @@ export default class Game extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            guesses: [],
+            response: 'Make your Guess',
+            answer: Math.floor(Math.random() * 100)
         };
     }
+
+    userGuess(guess) {
+        guess = Number(guess);
+        if(guess === 'NaN') {
+            throw new Error('Must enter a valid number');
+        }
+
+
+
+    }
+
+    resetGame() {
+        this.setState({
+            guesses: [],
+            response: 'Make your Guess',
+            correctAnswer: Math.floor(Math.random() * 100)
+        })
+    }
+
+
     render() {
         return (
             <section>
